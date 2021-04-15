1853851f3c28e967d9393df5c6373ab76d08983b
satoshi <3104nit@gmail.com>
2021-04-15 14:25:56 +0900

    allowed hosts

--- mypage/settings/production.py
+++ mypage/settings/production.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['mypage-api.herokuapp.com']
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
