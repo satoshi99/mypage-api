43713b5468a116c91e6b5b97f9a93c993eac2c58
satoshi <3104nit@gmail.com>
2021-04-16 03:06:28 +0900

    fix: url

--- mypage/urls.py
+++ mypage/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('mdeditor/', include('mdeditor.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
