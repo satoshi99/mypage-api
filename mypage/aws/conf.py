e4fdfc20d44e8caa068b499004d1d4afecefd7b4
satoshi <3104nit@gmail.com>
2021-04-16 14:21:48 +0900

    utils

--- mypage/aws/conf.py
+++ mypage/aws/conf.py
@@ -1,12 +1,12 @@
 import os
 
-DEFAULT_FILE_STORAGE = 'mypage.aws.utils.MediaRootS3BotoStorage'
+DEFAULT_FILE_STORAGE = 'mypage.aws.utils.MediaStorage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')  # 環境変数を指定
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')  # 環境変数を指定
 
 AWS_STORAGE_BUCKET_NAME = 'mypagemedia'  # Amazon S3 のバケット名
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',  # キャッシュの有効期限（最長期間）= 1日
 }
--- mypage/aws/utils.py
+++ mypage/aws/utils.py
@@ -1,4 +1,6 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-def MediaRootS3BotoStorage(): return S3Boto3Storage(location='media')
+class MediaStorage(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = False
